939ebc6cfa9f03f50b57655e3b03a313ad643282
auth-xyz <authenticover@gmail.com>
2024-08-21 02:24:50 -0400

    forgot to update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="palette-gen",
-    version="0.1.0",
+    version="0.1.1",
     author="auth-xyz",
     author_email="smmc.auth@gmail.com",
     description="A simple tool to extract and display color palettes from images.",
